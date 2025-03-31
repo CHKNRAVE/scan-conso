0de7d2b019252a66ceb6c7015687e895332e587f
Samuel HOUOT <houot.samuel@gmail.com>
2025-03-31 14:44:04 +0200

    Finalized the live picture input

--- src/PictureForm.jsx
+++ src/PictureForm.jsx
@@ -1,17 +1,18 @@
 import { BarcodeDetector } from "barcode-detector";
-import { createRef, useEffect, useState } from "react"
+import { createRef, useEffect, useRef, useState } from "react"
 import { createCode } from "./codes.mjs";
 
 export default function PictureForm() {
 	const [uploadedImage, setUploadedImage] = useState(null);
 	const [fileValidated, setFileValidated] = useState(false);
+	const [cameraModeAllowed, setCameraModePermission] = useState(false);
 	const [cameraModeEnabled, setCameraMode] = useState(false);
 	const [codeType, setCodeType] = useState(null);
 	const [codeValue, setCodeValue] = useState(null);
 	const [codeLabel, setCodeLabel] = useState("");
 
-
-	const submittedImageRef = createRef();
+	const videoElementRef = useRef(null);
+	const submittedImageRef = createRef(null);
 
 	useEffect(() => {
 		const beforeUnloadListener = function(ev) {
@@ -19,8 +20,37 @@ export default function PictureForm() {
 			ev.returnValue = "";
 		};
 		window.addEventListener("beforeunload", beforeUnloadListener);
+		
+		const userMediaAvailable = "getUserMedia" in navigator?.mediaDevices;
+		if(userMediaAvailable) navigator.permissions.query({name: "camera"}).then(cameraPermissionStatus => {
+			if(cameraPermissionStatus.state !== "denied") setCameraModePermission(true);
+		}, console.warn);
+
 		return () => window.removeEventListener("beforeunload", beforeUnloadListener);
 	}, []);
+
+	useEffect(() => {
+		if(!cameraModeEnabled) return;
+
+		let cameraSnapshotInterval;
+
+		navigator.mediaDevices.getUserMedia({video: {facingMode: {ideal: "environment"}}}).then(stream => {
+			videoElementRef.current.srcObject = stream;
+
+			cameraSnapshotInterval = setInterval(() => {
+				new BarcodeDetector({
+					formats: ["ean_13"]
+				}).detect(videoElementRef.current).then(data => {
+					if(!data.length) return;
+					const liveCodeType = data[0].format;
+					const liveCodeValue = Number(data[0].rawValue);
+					const liveCodeLabel = prompt(`Rentrez un nom pour le code-barres ${liveCodeValue}`);
+					if(liveCodeLabel !== null) createCode(liveCodeType, liveCodeValue, liveCodeLabel);
+				});
+			}, 500);
+		}, console.warn);
+		return () => clearInterval(cameraSnapshotInterval);
+	}, [cameraModeEnabled]);
 
 	const reset = function() {
 		setUploadedImage(null);
@@ -56,22 +86,19 @@ export default function PictureForm() {
 		reset();
 	};
 
-	console.log("Test");
-	navigator.permissions.query({name: "camera"}).then(console.log, console.warn);
-
 	const requestCameraAccess = function() {
+		navigator.mediaDevices.getUserMedia({video: {facingMode: {ideal: "environment"}}}).then(() => setCameraMode(true), console.warn);
 	};
 
 	return <>
 		<h2>Scanner un code</h2>
 		<div id="uploadField">
 			<input type="file" accept="/image" value={""} onInput={onFileInput} />
-			<div className="cameraDiv">
-				{cameraModeEnabled && <div>
-
-				</div>}
+			{cameraModeAllowed && <div className="cameraDiv">
+				<video ref={videoElementRef} width={300} height={300} autoPlay hidden={!cameraModeEnabled} />
+				{cameraModeEnabled && <p>L'image en direct est utilisée localement 2 fois par seconde.</p>}
 				{!cameraModeEnabled && <button onClick={requestCameraAccess}>Autoriser la caméra</button>}
-			</div>
+			</div>}
 			{!uploadedImage && !codeType && <p id="uploadInfo">
 				Vos photos ne sont ni conservées, ni mises en ligne.<br />
 				Le code-barres va être lu localement.<br />
