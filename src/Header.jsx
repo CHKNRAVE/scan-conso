84771bbdcbdb89d742cb43c85a59ecb5168f3c97
Samuel HOUOT <houot.samuel@gmail.com>
2025-04-22 10:46:49 +0200

    Made the page title a link

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import ScanButton from "./ScanButton";
 
 export default function Header({scanButton = true, openScanPanel}) {
 	return <header className="solidBlueBackground">
-		<h1 href="./" id="title">Scan Conso</h1>
+		<h1 id="title"><a href="./">Scan Conso</a></h1>
 		<InformationLink />
 		{scanButton && <ScanButton openScanPanel={openScanPanel}/>}
 	</header>
