2e491655aab4d3cd11fc5906869edfe18a7628bc
Samuel HOUOT <houot.samuel@gmail.com>
2025-04-22 14:14:55 +0200

    Added a changelog and edited the tutorial's tags

--- src/Tutorial.jsx
+++ src/Tutorial.jsx
@@ -1,9 +1,9 @@
 export default function Tutorial() {
 	return <div id="tutorial">
-		<h1>Bienvenue dans Scan Conso !</h1>
-		<h2><span>❶</span> Cliquez sur le bouton de scan ➚</h2>
-		<h2><span>❷</span> Envoyez une photo de votre code-barres, ou entrez-le manuellement.</h2>
-		<h2><span>❸</span> Revenez sur cette page principale, et vous trouverez le statut de tous vos codes-barres.</h2>
+		<h2>Bienvenue dans Scan Conso !</h2>
+		<h3><span>❶</span> Cliquez sur le bouton de scan ➚</h3>
+		<h3><span>❷</span> Envoyez une photo de votre code-barres, ou entrez-le manuellement.</h3>
+		<h3><span>❸</span> Revenez sur cette page principale, et vous trouverez le statut de tous vos codes-barres.</h3>
 		<p>Scan Conso n'utilise pas de cookie, cependant son fonctionnement déclenche l'envoi de cookies par certains sites gouv.fr, dès la première recherche.<br />En enregistrant votre premier code-barres, vous acceptez leur enregistrement qui n'est pas exploité par Scan Conso.</p>
 	</div>
 };
